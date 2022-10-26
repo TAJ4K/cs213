3aeecaf011d5ce1bc2fc3085488dfed11cc30693
TAJ4K <76080011+TAJ4K@users.noreply.github.com>
2022-10-26 18:00:46 -0400

    inclass 10/26 pt2

--- inclass/inclass.js
+++ inclass/inclass.js
@@ -1,3 +1,1 @@
-let prompt = require('prompt-sync')();
-let a = prompt("Enter the first number: "), b = prompt("Enter the second number: ");
-parseInt(a)*parseInt(b)==parseInt(prompt(`Enter the result of ${a}*${b}: `)) ? console.log("Your answer is correct!") : console.log(`Your answer is wrong!\nThe correct answer is ${parseInt(a)*parseInt(b)}`);
+require('prompt-sync')()("Enter a number: ") % 2 == 0 ? console.log("The number you entered is even") : console.log("The number you entered is odd");
