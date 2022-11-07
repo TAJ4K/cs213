06eb12f40ee5fa8a4ae177ae28f0bec4601ad807
TAJ4K <76080011+TAJ4K@users.noreply.github.com>
2022-11-07 17:07:33 -0500

    varname

--- assignment_4/assignment_4.js
+++ assignment_4/assignment_4.js
@@ -1,11 +1,11 @@
 const prompt = require('prompt-sync')();
 console.log("===== Input Commuting Costs =====");
-let cost = prompt("What is your daily round trip? ");
+let trip = prompt("What is your daily round trip? ");
 let days = prompt("How many days per month do you commute? ");
 let parking = prompt("How much do you pay for parking per month? ");
 
 const ppm = 0.8236
-let monthly = (parseFloat(cost) * ppm) * days + parseFloat(parking);
+let monthly = (parseFloat(trip) * ppm) * days + parseFloat(parking);
 console.log("===== Cost of Commuting =====");
 console.log("Your estimated monthly cost of commuting is $" + parseFloat(monthly).toFixed(2));
 console.log("Your estimated yearly cost of commuting is $" + parseFloat(monthly * 12).toFixed(2));
