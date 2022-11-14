e7295a1d0d0434c68134172897a5a492c4341080
TAJ4K <76080011+TAJ4K@users.noreply.github.com>
2022-11-14 17:30:07 -0500

    variable names

--- stringquiz/q2.js
+++ stringquiz/q2.js
@@ -1,9 +1,9 @@
 const prompt = require("prompt-sync")();
 
-let str = prompt("Enter the main string: ");
+let mainstr = prompt("Enter the main string: ");
 let sub = prompt("Enter the substring: ");
 let repl = prompt("Enter the replacement string: ");
 
-str.includes(sub) ?
-    console.log("The final string is", str.replace(sub, repl)) :
+mainstr.includes(sub) ?
+    console.log("The final string is", mainstr.replace(sub, repl)) :
     console.log("ERROR: The substring you entered does not exist in the string");
